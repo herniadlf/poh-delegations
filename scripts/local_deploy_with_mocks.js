c6576649e8619ea92b98d810de140ef3b55656a4
Hernán de la Fuente <hernan.de@mercadolibre.com>
2022-09-21 23:15:18 -0300

    add local deploy with mocks scripts

---
+++ scripts/local_deploy_with_mocks.js
@@ -0,0 +1,26 @@
+async function main() {
+    let contractFactory = await ethers.getContractFactory('DelegateRegistryMock');
+    const delegationRegistry = await contractFactory.deploy();
+    await delegationRegistry.deployed();
+
+    contractFactory = await ethers.getContractFactory('PoHMock');
+    const pohMock = await contractFactory.deploy();
+    await pohMock.deployed();
+    
+    contractFactory = await ethers.getContractFactory('POHDelegationDecay');
+    const delegationDecay = await contractFactory.deploy(
+        pohMock.address, 
+        delegationRegistry.address,
+        '0x706f682e65746800000000000000000000000000000000000000000000000000'
+    );
+    await delegationDecay.deployed();
+
+    console.log('POHDelegationDecay Contract address ' + delegationDecay.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
